700d77c8861ea36a6462caadff3991eab6960abf
C-zx <853695319@qq.com>
2019-07-20 19:09:40 +0800

    使用通用的基于类的视图

--- snippets/views.py
+++ snippets/views.py
@@ -10,38 +10,19 @@ return Response(data)  # 渲染成客户端请求的内容类型。
 request.data可以处理传入的json请求，但它也可以处理其他格式。
 同样，我们返回带有数据的响应对象，但允许REST框架将响应给我们渲染成正确的内容类型。
 """
+from rest_framework import generics
+
 from .models import Snippet
 from .serializers import SnippetModelSerializer
-from rest_framework import mixins, generics
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """列出所有code snippet，或创建一个新的snippet。"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetModelSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)  # mixins.ListModelMixin
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)  # mixins.CreateModelMixin
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """获取，更新或删除一个 code snippet。"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetModelSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)  # mixins.RetrieveModelMixin
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)  # mixins.UpdateModelMixin
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)  # mixins.DestroyModelMixin
