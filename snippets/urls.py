f9917b99b170ce2bbfc8d3d386634a20c4f161c7
C-zx <853695319@qq.com>
2019-07-21 02:23:13 +0800

    显式地将ViewSets绑定到URL
    修复小bug：renderer_classes漏了最后的es导致异常无法识别的renderer_class

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from . import views
+from rest_framework import renderers
+
+snippet_list = views.SnippetViewSet.as_view(actions={'get': 'list', 'post': 'create'})
+snippet_detail = views.SnippetViewSet.as_view(actions={
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = views.SnippetViewSet.as_view(
+    actions={'get': 'highlight'},
+    renderer_classes=[renderers.StaticHTMLRenderer]
+)
+
 
 urlpatterns = [
-    path('', views.SnippetList.as_view(), name='snippet-list'),
-    path('<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
+    path('', snippet_list, name='snippet-list'),
+    path('<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('<int:pk>/highlight/', snippet_highlight, name='snippet-highlight')
 ]
 
